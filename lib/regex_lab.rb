c713c10bf0afec7e8221f26a6d1f6aaef5fba20c
Marc Mills <mdmills1990@gmail.com>
2018-08-15 03:29:51 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-word.scan(/\b[AEIOUaeiou][a-z]*\b/)
+if word.scan(/\b[AEIOUaeiou][a-z]*\b/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+phone.scan(/[\b\W*\d]{10}/)
 end
