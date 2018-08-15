48a9b8128de4b6f9a0bb01c5a64e8b647984d931
Marc Mills <mdmills1990@gmail.com>
2018-08-15 03:36:11 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,4 +1,21 @@
 def starts_with_a_vowel?(word)
-if word.scan(/\b[AEIOUaeiou][a-z]*\b/).length >= 0
-  return true
+vowel_array = word.scan(/\b[AEIOUaeiou][a-z]*\b/)
+if vowel_array.length > 0
+  true
+end
+
+def words_starting_with_un_and_ending_with_ing(text)
+text.scan(/[u][n][a-zA-Z]*[i][n][g]/)
+end
+
+def words_five_letters_long(text)
+text.scan(/\b[A-Za-z]{5}\b/)
+end
+
+def first_word_capitalized_and_ends_with_punctuation?(text)
+
+end
+
+def valid_phone_number?(phone)
+phone.scan(/[\b\W*\d]{10}/)
 end
