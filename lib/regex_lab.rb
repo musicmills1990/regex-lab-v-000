908c542008d56b90887a6ce985b4113de91e7e75
Marc Mills <mdmills1990@gmail.com>
2018-08-15 03:52:56 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,34 @@
 def starts_with_a_vowel?(word)
-
+vowel_array = word.scan(/\b[AEIOUaeiou][a-z]*\b/)
+if vowel_array.length == 0
+  false
+else
+  true
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/[u][n][a-zA-Z]*[i][n][g]/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b[A-Za-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+correct_sentence = text.scan(/\A[A-Z][\w\W]*[.?!]\z/)
+if correct_sentence.length == 0
+  false
+else
+  true
+end
 end
 
 def valid_phone_number?(phone)
-
+phone_array = phone.scan(/[\b\W*\d]{10}/)
+if phone_array.length == 0
+  false
+else
+  true
+end
 end
