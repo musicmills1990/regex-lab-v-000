0975b474266da3956679a71e16fa163cbd65f27b
Marc Mills <mdmills1990@gmail.com>
2018-08-15 01:25:32 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,6 @@
 def starts_with_a_vowel?(word)
-
+"afoot Excellent incredible Absolute unreal Inconceivable octopus chandoo Crenel crooked Flimsy folksy Dog Hermit
+makeshift Palazzi sixfold Truth".scan(/aeiou/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
