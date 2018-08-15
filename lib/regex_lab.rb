7677b0f2c7e631363177fd20321d84610d3342ec
Marc Mills <mdmills1990@gmail.com>
2018-08-15 02:46:01 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-if word.scan(/\b[AEIOUaeiou][a-z]*\b/)
+word.scan(/\b[AEIOUaeiou][a-z]*\b/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -7,7 +7,7 @@ word.scan(/\b[)
 end
 
 def words_five_letters_long(text)
-
+word.scan(/\b[A-Za-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
